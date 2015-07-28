ac4c5ce68c58a289287369e941f0bc09a5cc0b9e
KTDS_Christof_Kim <taehun3718@gmail.com>
2015-07-28 14:30:03 +0900

    commit?

--- app/src/main/java/com/ktds/myapplication/MainActivity.java
+++ app/src/main/java/com/ktds/myapplication/MainActivity.java
@@ -12,13 +12,10 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Hello world
+        //Hello worldfdawf asd
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    public void xxx() {
+        System.out.println("aaaaa");
     }
 }
